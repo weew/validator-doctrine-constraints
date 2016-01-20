65774267dc7f63c5795e1d803dd41575e39b21e9
Maxim Kott <maximkott@gmail.com>
2016-01-20 17:53:43 +0200

    Adding short description.

--- src/Weew/Validator/DoctrineConstraints/UniqueConstraint.php
+++ src/Weew/Validator/DoctrineConstraints/UniqueConstraint.php
@@ -5,6 +5,9 @@ namespace Weew\Validator\DoctrineConstraints;
 use Doctrine\Common\Persistence\ObjectRepository;
 use Weew\Validator\IConstraint;
 
+/**
+ * Ensure that a column value is unique.
+ */
 class UniqueConstraint implements IConstraint {
     /**
      * @var
